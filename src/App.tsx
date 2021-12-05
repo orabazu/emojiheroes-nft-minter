b78fdb8d734c67b2eff1ed6f40f63bd2cc34ad90
zhunor <ozerorhun@gmail.com>
2021-12-05 23:45:42 +0300

    Revert twitter

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import "./styles/App.css";
 import twitterLogo from "./assets/twitter-logo.svg";
+// import React, { useEffect, useState } from "react";
+
 
 import { Main } from "./components/Main";
 import { AccountContextProvider } from "./contexts/accountContext";
+import { TWITTER_HANDLE, TWITTER_LINK } from "./const";
+
+// Constants
 
 
 const App = () => {
@@ -11,6 +16,19 @@ const App = () => {
       <div className="container">
         <AccountContextProvider>
           <Main />
+          <div className="footer-container">
+            <img
+              alt="Twitter Logo"
+              className="twitter-logo"
+              src={twitterLogo}
+            />
+            <a
+              className="footer-text"
+              href={TWITTER_LINK}
+              target="_blank"
+              rel="noreferrer"
+            >{`built on @${TWITTER_HANDLE}`}</a>
+          </div>
         </AccountContextProvider>
       </div>
     </div>
